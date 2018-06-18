46ff497f1bdf0c3c5a1c93b12830c9e42c5cf42e
Cristina <cristgl@correo.ugr.es>
2018-06-18 18:53:02 +0200

    added license

--- pick_and_place/baxter_img.cpp
+++ pick_and_place/baxter_img.cpp
@@ -1,6 +1,15 @@
 // Copyright (C) 2017 Maik Heufekes, 05/07/2017.
 // License, GNU LGPL, free software, without any warranty.
 
+/* Copyright (C) 2018  Cristina Garrido
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details. */
+
 #include "ros/ros.h"
 #include "sensor_msgs/Image.h"
 #include <sstream>
